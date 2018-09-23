2a7608e618133738989389b4f84e90474fe69c0d
alexander-schott <32785666+alexander-schott@users.noreply.github.com>
2018-09-23 16:04:30 -0400

    Successfully checks for correct login and navigates to MainActivity.

--- app/src/main/java/spacepirates/breadbox/LoginActivity.java
+++ app/src/main/java/spacepirates/breadbox/LoginActivity.java
@@ -7,9 +7,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -26,14 +28,20 @@ public class LoginActivity extends AppCompatActivity {
 
         final String username = "user";
         final String password = "pass";
+        final String failedLoginMessage = "Incorrect username or password.";
 
         loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-                if (usernameField.getText().equals(username)
-                        && passwordField.getText().equals(password)) {
+                if (usernameField.getText().toString().equals(username)
+                        && passwordField.getText().toString().equals(password)) {
                     Context context = view.getContext();
                     Intent intent = new Intent(context, MainActivity.class);
                     context.startActivity(intent);
+                } else {
+                    Toast failMessage = Toast.makeText(getApplicationContext(), failedLoginMessage,
+                            Toast.LENGTH_SHORT);
+                    failMessage.show();
                 }
         }}
         );
