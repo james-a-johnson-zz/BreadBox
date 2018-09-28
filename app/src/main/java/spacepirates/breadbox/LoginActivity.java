6b5a782932f7c3bd78bd74de70b8e29e5fdc4c2b
James Johnson <jjohnson474@gatech.edu>
2018-09-28 11:45:00 -0400

    User can sign in and register using Firebase.

--- app/src/main/java/spacepirates/breadbox/LoginActivity.java
+++ app/src/main/java/spacepirates/breadbox/LoginActivity.java
@@ -3,49 +3,67 @@ package spacepirates.breadbox;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText usernameField;
+    private EditText passwordField;
+    private Button loginButton;
+    private Button registerButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
+        usernameField = findViewById(R.id.UsernameField);
+        passwordField = findViewById(R.id.PasswordField);
+        loginButton = findViewById(R.id.LoginButton);
+        registerButton = findViewById(R.id.RegisterButton);
 
-        final EditText usernameField = findViewById(R.id.UsernameField);
-        final EditText passwordField = findViewById(R.id.PasswordField);
-        Button loginButton = findViewById(R.id.LoginButton);
-        Button registerButton = findViewById(R.id.RegisterButton);
-
-        final String username = "user";
-        final String password = "pass";
-        final String failedLoginMessage = getString(R.string.login_fail_message);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (usernameField.getText().toString().equals(username)
-                        && passwordField.getText().toString().equals(password)) {
-                    Context context = view.getContext();
-                    Intent intent = new Intent(context, MainActivity.class);
-                    context.startActivity(intent);
-                } else {
-                    Toast failMessage = Toast.makeText(getApplicationContext(), failedLoginMessage,
-                            Toast.LENGTH_SHORT);
-                    failMessage.show();
-                }
-        }}
-        );
-
+        loginButton.setOnClickListener(this);
+        registerButton.setOnClickListener(this);
     }
 
+    private void logIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Context context = getApplicationContext();
+                            Intent intent = new Intent(context, MainActivity.class);
+                            context.startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.LoginButton) {
+            logIn(usernameField.getText().toString(), passwordField.getText().toString());
+        } else if (i == R.id.RegisterButton) {
+            Context context = getApplicationContext();
+            Intent intent = new Intent(context, Register.class);
+            context.startActivity(intent);
+        }
+    }
 }
---
+++ app/src/main/java/spacepirates/breadbox/Register.java
@@ -0,0 +1,87 @@
+package spacepirates.breadbox;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import spacepirates.breadbox.model.UserType;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
+    private Button register;
+    private Spinner userType;
+    private EditText emailText;
+    private EditText passwordText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        register = findViewById(R.id.RegisterButton);
+        userType = findViewById(R.id.UserTypeSpinner);
+        emailText = findViewById(R.id.EmailText);
+        passwordText = findViewById(R.id.PasswordText);
+
+        userType.setAdapter(new ArrayAdapter<UserType>(this, android.R.layout.simple_spinner_item, UserType.values()));
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String email = emailText.getText().toString();
+        String pass = passwordText.getText().toString();
+        UserType ut;
+        switch (userType.getSelectedItem().toString()) {
+            case "Guest":
+                ut = UserType.GUEST;
+                break;
+            case "Basic":
+                ut = UserType.BASIC;
+                break;
+            case "Location Employee":
+                ut = UserType.LOCATION_EMPLOYEE;
+                break;
+            case "Manager":
+                ut = UserType.MANAGER;
+                break;
+            case "Administrator":
+                ut = UserType.ADMINISTRATOR;
+                break;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // TODO: Need to add user to a user database that includes the type
+                            // TODO: Include name and location depending on employee type?
+                            // Sign in success, update UI with the signed-in user's information
+                            Context context = getApplicationContext();
+                            Intent intent = new Intent(context, MainActivity.class);
+                            context.startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Register.this, "Registering user failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
