e1afdf9bffeca47e056457a2be3c029d9798fe9b
Logan Schick <schicklogan@gmail.com>
2018-10-05 12:53:18 -0400

    Added getters and setters to Location. LocationDatabase correctly takes in a csv to initialize the database and more locations can be added through the addLocation method.

--- app/src/main/java/spacepirates/breadbox/Location.java
+++ app/src/main/java/spacepirates/breadbox/Location.java
@@ -2,22 +2,86 @@ public class Location {
 
     private String name;
     private String type;
-    private double longitude;
     private double latitude;
+    private double longitude;
     private String address;
     private String phoneNumber;
 
-    public Location(String name, String type, String longitude, String latitude, String address, String phoneNumber) {
+    public Location(String name, String type, double latitude, double longitude, String address, String phoneNumber) {
         this.name = name;
         this.type = type;
-        this.longitude = new Double(longitude);
-        this.latitude = new Double(latitude);
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.address = address;
         this.phoneNumber = phoneNumber;
     }
 
+    public Location(String name, String type, String latitude, String longitude, String address, String phoneNumber) {
+        this(name, type, new Double(latitude), new Double(longitude), address, phoneNumber);
+    }
+
     //Only use with correctly formatted csv
     public Location(String[] arr) {
-        Location(arr[1], arr[8], arr[3], arr[2], arr[4], arr[9]);
+        this(arr[1], arr[8], arr[2], arr[3], arr[4], arr[9]);
+    }
+
+    public String toString() {
+        return (type + ": " + name + " at " + address + ". Call " + phoneNumber);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = new Double(latitude);
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = new Double(longitude);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
--- app/src/main/java/spacepirates/breadbox/LocationDatabase.java
+++ app/src/main/java/spacepirates/breadbox/LocationDatabase.java
@@ -2,18 +2,37 @@ import java.util.Scanner;
 import java.io.File;
 import java.util.ArrayList;
 
-public static class LocationDatabase {
+public class LocationDatabase {
 
     private static ArrayList<Location> locations;
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(new File("LocationData.csv"));
-        ArrayList<String[]> lines = new ArrayList<>();
-        while(scan.hasNextLine()) {
-            lines.add(scan.getNextLine().split(","));
+        try {
+            Scanner scan = new Scanner(new File("LocationData.csv"));
+            scan.nextLine(); //Skipping line that indicates the format of the CSV
+            ArrayList<String[]> lines = new ArrayList<>();
+            while(scan.hasNextLine()) {
+                lines.add(scan.nextLine().split(","));
+            }
+            locations = new ArrayList<Location>();
+            for (String[] arr : lines) {
+                if (arr.length == 11) {
+                    locations.add(new Location(arr));
+                }
+            }
+
+            addLocation("Test Store", "Store", "45.5", "0", "1111 Fake Street", "(555) 555 - 5555");
+
+            for (Location l : locations) {
+                System.out.println(l);
+            }
+        } catch(Exception e) {
+            System.out.println(e);
+            System.out.println("Hit an exception");
         }
-        locations = new ArrayList<Location>();
-        for (String[] arr : lines) {
-            locations.add(new Location(arr));
-        }
+    }
+
+    public static void addLocation(String name, String type, String latitude, String longitude, String address, String phoneNumber) {
+        locations.add(new Location(name, type, latitude, longitude, address, phoneNumber));
+    }
 }
