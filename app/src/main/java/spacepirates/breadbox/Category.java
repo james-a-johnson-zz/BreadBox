6a2eddab6b86a6836f8bea78f29c31ae708d8576
CamilleBossut <cbossut21@gatech.edu>
2018-10-08 13:17:25 -0400

    correcting merge conflicts

--- app/src/main/java/spacepirates/breadbox/Category.java
+++ app/src/main/java/spacepirates/breadbox/Category.java
@@ -1,10 +1,6 @@
 package spacepirates.breadbox;
 
-<<<<<<< HEAD
 public enum Category{
-=======
-public enum Category {
->>>>>>> 0720a98114055db3a77324947a57c7359c705b9d
     APPAREL("Apparel"), FURNITURE("Furniture"), KITCHENWARE("Kitchenware"),
      TOY("Toy"), BOOK("Book"), ELECTRONICS("Electronics");
 
--- app/src/main/java/spacepirates/breadbox/Tag.java
+++ app/src/main/java/spacepirates/breadbox/Tag.java
@@ -18,9 +18,7 @@ public enum Tag{
     ELECTRONICS("Electronics", Category.KITCHENWARE, Category.FURNITURE, Category.ELECTRONICS)
 
     NOVEL("Novel", Category.BOOK), BIOGRAPHY("Biography", Category.BOOK),
-    COOKBOOK("CookBook" Category.BOOK, Category.KITCHENWARE)
-
-    ;
+    COOKBOOK("CookBook" Category.BOOK, Category.KITCHENWARE);
 
     private String name;
     private List<Category> categories;
