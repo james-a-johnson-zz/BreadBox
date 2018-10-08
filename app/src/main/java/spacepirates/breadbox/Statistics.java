76f673ce91e9e4f168ba00a03df0400b7e31ad3c
CamilleBossut <cbossut21@gatech.edu>
2018-10-08 15:46:44 -0400

    Modify Statistics constructor to call an equivalent but reusable update method

--- app/src/main/java/spacepirates/breadbox/Statistics.java
+++ app/src/main/java/spacepirates/breadbox/Statistics.java
@@ -13,22 +13,42 @@ public class Statistics{
     // Donations per month per location.
 
     //each location has-a statistics
+    //each business has-a statistics
 
-    int donationRate; //in donations/month
-    List<Integer> donationRatesYearly;
+    //item tracking info
+    int donationRate; //in donations/this month
+    List<Integer> donationRatesMonthly;
 
     int turnoverRate; //avg time items spend in inventory at location
 
-    int distributionRate; //in sales/month
-    List<Integer> distributionRatesYearly;
+    int distributionRate; //in sales/this month
+    List<Integer> distributionRatesMonthly;
 
     int dailyDonations;
     int dailyDistributions;
 
-    int inventorySize;
-    int[] categoryInventorySize;
+    //inventory sizes:
+    int inventorySize; //current size
+    int[] categoryInventorySize; //current size per category
+
+    //pricing:
+    int income //in income/this month
+    List<Integer> incomeMonthly;
+
+    int inventoryValue //in inventoryValue/this month
+    List<Integer> inventoryValueMonthly;
 
     public Statistics(Location location){
+        this.update(location);
+    }
+
+    public Statistics(Business business){
+        this.update(business);
+
+    }
+
+    public void update(Location location){
+
         locInventory = location.getInventory();
         inventorySize = locInventory.size();
         //this.updateCategoryInventories(locInventory); --> could make this method, but going to make
@@ -53,21 +73,13 @@ public class Statistics{
             dailyDonations++;
             i--;
         }
+
         //update daily distributions
-        //update donation rate & donation rate yearly
-        //update distribution rate & distribution rate yearly
+        //update donation rate & donation rate monthly list
+        //update distribution rate & distribution rate monthly list
         //update turnover rate
-
-
-
-    }
-
-    public Statistics(Business business){
-
-    }
-
-    public void update(Location location){
-
+        //update income & income monthly list
+        //update inventory value & inventory value monthly list
     }
 
     public void update(Business business){
