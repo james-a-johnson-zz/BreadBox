a396fb2c594c7c51ae09215620e1f663b56186f0
James <jjohnsonjj1251@gmail.com>
2018-10-14 16:01:41 -0400

    Master branch now uses firebase for user authentication.
    Signed-off-by: James Johnson <jjohnson474@gatech.edu>

--- app/src/main/java/spacepirates/breadbox/LoginActivity.java
+++ app/src/main/java/spacepirates/breadbox/LoginActivity.java
@@ -3,24 +3,27 @@ package spacepirates.breadbox;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.google.firebase.FirebaseApp;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    //FirebaseAuth firebase = FirebaseAuth.getInstance();
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //FirebaseAPP.initializeApp(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        firebaseAuth = FirebaseAuth.getInstance();
 
         final EditText usernameField = findViewById(R.id.UsernameField);
         final EditText passwordField = findViewById(R.id.PasswordField);
@@ -38,18 +41,22 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (usernameField.getText().toString().equals(username)
-                        && passwordField.getText().toString().equals(password)) {
-                    Context context = view.getContext();
-                    Intent intent = new Intent(context, MainActivity.class);
-                    context.startActivity(intent);
-                } else {
-                    Toast failMessage = Toast.makeText(getApplicationContext(), failedLoginMessage,
-                            Toast.LENGTH_SHORT);
-                    failMessage.show();
-                }
-        }}
-        );
+                firebaseAuth.signInWithEmailAndPassword(usernameField.getText().toString(),
+                        passwordField.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Context context = getApplicationContext();
+                                    Intent intent = new Intent(context, MainActivity.class);
+                                    context.startActivity(intent);
+                                } else {
+                                    Toast.makeText(getApplicationContext(), failedLoginMessage, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,5 +77,4 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
-
 }
