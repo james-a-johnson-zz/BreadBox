c57d22a3e94846e5dd815b438a0334b2a7a1d6e5
alexander-schott <32785666+alexander-schott@users.noreply.github.com>
2018-10-28 13:30:53 -0400

    Merge branches 'Alexander' and 'master' of https://github.com/james-a-johnson/BreadBox into Alexander
    undid Logan's lambda expression and made it an anonymous inner class.
    # Conflicts:
    #	local.properties

--- app/src/main/java/spacepirates/breadbox/LocationsActivity.java
+++ app/src/main/java/spacepirates/breadbox/LocationsActivity.java
@@ -1,11 +1,16 @@
 package spacepirates.breadbox;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -20,6 +25,41 @@ public class LocationsActivity extends AppCompatActivity {
 
         Model model = Model.getInstance();
         ArrayList<Location> locations;
+
+        final DrawerLayout mDrawerLayout;
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        /**
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = new NavigationView(getApplicationContext());
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                // set item as selected to persist highlight
+                menuItem.setChecked(true);
+                // close drawer when item is tapped
+                mDrawerLayout.closeDrawers();
+
+                int selected = menuItem.getItemId();
+                Intent intent = null;
+                Context context = getApplicationContext();
+                switch(selected) {
+                    case R.id.drawer_menu_locations:
+                        intent = new Intent(getApplicationContext(), LocationsActivity.class);
+                        break;
+                    case R.id.drawer_menu_logout:
+                        //TODO need a logout acitivity / method in model
+                        intent = new Intent(getApplicationContext(), LoginActivity.class);
+                        break;
+                }
+                //or not current activity?
+                if (intent != null) {
+                    context.startActivity(intent);
+                }
+                return true;
+            }
+        });
+         **/
+
 
         // Checks that location database is initialized and populates it if it is not.
         try {
