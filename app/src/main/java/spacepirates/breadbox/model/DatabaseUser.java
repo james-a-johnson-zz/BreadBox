62408171c27fa80da54b83bf9733ce4f56e13efe
mburke1434 <31456608+mburke1434@users.noreply.github.com>
2018-11-06 20:15:04 -0500

    Added Javadocs to Category and DatabaseUser

--- app/src/main/java/spacepirates/breadbox/model/DatabaseUser.java
+++ app/src/main/java/spacepirates/breadbox/model/DatabaseUser.java
@@ -5,41 +5,62 @@ public class DatabaseUser {
     public UserType type;
     public String locus;
 
-    public DatabaseUser() {
-    }
-
+    /**
+     * Constructor for a user that utilizes the databases and is not provided a location
+     * @param username      Self explanatory
+     * @param type          User type
+     */
     public DatabaseUser(String username, UserType type) {
         this.username = username;
         this.type = type;
         locus = "NULL";
     }
 
+    /**
+     * Same as above but has a specific location tied to them
+     * @param username      Self explanatory
+     * @param type          User type
+     */
     public DatabaseUser(String username, UserType type, String locus) {
         this(username, type);
         this.locus = locus;
     }
 
+    /**
+     * Getter for the username
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Setter for username
+     * @param username     new data to set username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * @return type     Type of user
+     */
     public UserType getType() {
         return type;
     }
 
+    /**
+     * @param type Set the usertype to a new type
+     */
     public void setType(UserType type) {
         this.type = type;
     }
 
-    public String getLocus() {
-        return locus;
-    }
-
-    public void setLocus(String locus) {
-        this.locus = locus;
-    }
+//    public String getLocus() {
+//        return locus;
+//    }
+//
+//    public void setLocus(String locus) {
+//        this.locus = locus;
+//    }
 }
