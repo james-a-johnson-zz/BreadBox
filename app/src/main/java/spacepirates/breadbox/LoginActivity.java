d97bbd218a3d639e2936c0678f1d58bacb8d3d19
mburke1434 <31456608+mburke1434@users.noreply.github.com>
2018-11-14 21:04:41 -0500

    Fixed an app-crashing cast error I made to shut up checker

--- app/src/main/java/spacepirates/breadbox/LoginActivity.java
+++ app/src/main/java/spacepirates/breadbox/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableStringBuilder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
+import io.opencensus.trace.Span;
 import spacepirates.breadbox.model.Admin;
 import spacepirates.breadbox.model.BasicUser;
 import spacepirates.breadbox.model.DatabaseUser;
@@ -65,8 +67,8 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText userText = (EditText) usernameField.getText();
-                EditText passText = (EditText) passwordField.getText();
+                SpannableStringBuilder userText = (SpannableStringBuilder) usernameField.getText();
+                SpannableStringBuilder passText = (SpannableStringBuilder) passwordField.getText();
                 firebaseAuth.signInWithEmailAndPassword(userText.toString(),
                         passText.toString())
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
